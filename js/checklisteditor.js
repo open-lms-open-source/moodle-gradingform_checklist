ef9611f23d5adfdafe1a6eac9a71e85cf5d9c457
Sam Chaffee <sam@moodlerooms.com>
2012-05-15 16:59:25 -0400

    INT-3358, INT-3364: Adding Moodleform checklist element definition and basic corresponding javascript

---
+++ js/checklisteditor.js
@@ -0,0 +1,13 @@
+M.gradingform_checklisteditor = {'templates' : {}, 'eventhandler' : null, 'name' : null, 'Y' : null};
+
+/**
+ * This function is called for each checklisteditor on page.
+ */
+M.gradingform_checklisteditor.init = function(Y, options) {
+    M.gradingform_checklisteditor.name = options.name
+    M.gradingform_checklisteditor.Y = Y
+    M.gradingform_checklisteditor.templates[options.name] = {
+        'criterion' : options.criteriontemplate,
+        'level' : options.leveltemplate
+    }
+};
