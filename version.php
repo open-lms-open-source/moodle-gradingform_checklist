e476edb8920510375a85214dbc2da905af62341d
Mark Nielsen <mark@moodlerooms.com>
2016-12-13 14:00:26 -0800

    INT-10252: bump plugin version, requires and release for Moodle 3.1

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2016012600;
-$plugin->requires   = 2015111604; // Requires this Moodle version (3.0.4).
-$plugin->release    = '3.0.4 (Build: 20160509)';
+$plugin->version    = 2016121300;
+$plugin->requires   = 2016052300;
+$plugin->release    = '3.1.3';
 $plugin->maturity   = MATURITY_STABLE;
