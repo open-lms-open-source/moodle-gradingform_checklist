c48a4fcc4eb0770777e806e1d090ebeb649a3dfd
Mark Nielsen <mark@moodlerooms.com>
2017-12-08 15:42:58 -0800

    INT-12724: Bump version and release

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2017063000;
+$plugin->version    = 2017120800;
 $plugin->requires   = 2017051500;
-$plugin->release    = '3.3.1';
+$plugin->release    = '3.3.3';
 $plugin->maturity   = MATURITY_STABLE;
