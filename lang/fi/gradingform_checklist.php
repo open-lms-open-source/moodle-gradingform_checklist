40a6f52a2488983652a1bf8cf46ae03e4112b67a
David Castro <david.castro@openlms.net>
2021-02-08 06:46:50 +0000

    INT-16713: Importing fi lang

--- lang/fi/gradingform_checklist.php
+++ lang/fi/gradingform_checklist.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2020 Blackboard Inc. (http://www.blackboard.com)
+ * @copyright  Copyright (c) 2021 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -23,21 +23,18 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['addgroup'] = 'Lisää ryhmä';
 $string['alwaysshowdefinition'] = 'Salli käyttäjien esikatsella moduulissa käytettyä tarkistuslistaa (muuten tarkistuslista tulee nähtäväksi vasta arvioinnin jälkeen)';
-$string['backtoediting'] = 'Takaisin muokkaamaan';
-$string['checked'] = 'Tarkistettu';
+$string['backtoediting'] = 'Takaisin muokkaukseen';
+$string['checked'] = 'Valittu';
 $string['checkitem'] = 'Merkitse täydet pisteet kohteelle {$a}';
 $string['checklist'] = 'Tarkistuslista';
 $string['checklistmapping'] = 'Pisteiden arvosanoihin yhdistämisen säännöt';
-$string['checklistmappingexplained'] = 'Tämän tarkistuslistan pienin mahdollinen pistemäärä on <b>{$a->minscore} pistettä</b> ja se muunnetaan tämän moduulin alimmaksi mahdolliseksi arvosanaksi (joka on nolla, ellei asteikko ole käytössä).
-    Suurin mahdollinen pistemäärä <b>{$a->maxscore} pistettä</b> muunnetaan korkeimmaksi arvosanaksi.<br />
-    Muut pistemäärät muunnetaan ja pyöristetään lähimpään vastaavaan arvosanaan.<br />
-    Jos arvosanan sijaan käytetään asteikkoa, pisteet muunnetaan skaalan elementteihin aivan kuin peräkkäisinä kokonaislukuina.';
+$string['checklistmappingexplained'] = 'Tämän tarkistuslistan pienin mahdollinen pistemäärä on <b>{$a->minscore} pistettä</b> ja se muunnetaan tämän moduulin alimmaksi mahdolliseksi arvosanaksi (joka on nolla, ellei asteikko ole käytössä).      Suurin mahdollinen pistemäärä <b>{$a->maxscore} pistettä</b> muunnetaan korkeimmaksi arvosanaksi.<br />      Muut pistemäärät muunnetaan ja pyöristetään lähimpään vastaavaan arvosanaan.<br />      Jos arvosanan sijaan käytetään asteikkoa, pisteet muunnetaan skaalan elementteihin aivan kuin peräkkäisinä kokonaislukuina.';
 $string['checklistoptions'] = 'Tarkistuslistan asetukset';
 $string['checkliststatus'] = 'Tarkistuslistan tilanne';
 $string['confirmdeletegroup'] = 'Haluatko varmasti poistaa tämän ryhmän?';
-$string['confirmdeleteitem'] = 'Haluatko varmasti poistaa tämän?';
+$string['confirmdeleteitem'] = 'Oletko varma, että haluat poistaa tämän?';
 $string['definechecklist'] = 'Määritä tarkistuslista';
-$string['description'] = 'Kuvaus';
+$string['description'] = 'Keskustelualueen kuvaus';
 $string['err_definitionmax'] = 'Kohteiden määritelmissä voi olla enintään 255 merkkiä';
 $string['err_descriptionmax'] = 'Ryhmän kuvauksessa voi olla enintään 255 merkkiä';
 $string['err_nodefinition'] = 'Kohteen määritelmä ei voi olla tyhjä';
@@ -62,16 +59,15 @@ $string['itemempty'] = 'Muokkaa kohdetta napsauttamalla';
 $string['itemfeedback'] = 'Palaute: {$a}';
 $string['itemremark'] = 'Kohteen huomautus: {$a}';
 $string['itemscore'] = 'Kohteen pisteet';
-$string['name'] = 'Nimi';
+$string['name'] = 'Minkä nimen haluat antaa tälle sivulle?';
 $string['needregrademessage'] = 'Tarkistuslistan määritelmää on muokattu tämän opiskelijan arvioinnin jälkeen. Ko. opiskelija ei voi nähdä tarkistuslistaa, ennen kuin tarkistat tarkistuslistan ja päivität arvosanan.';
 $string['pluginname'] = 'Tarkistuslista';
 $string['previewchecklist'] = 'Esikatsele tarkistuslistaa';
 $string['overallpoints'] = 'Kokonaispisteet';
-$string['regrademessage1'] = 'Olet tallentamassa muutoksia tarkistuslistaan, jota on jo käytetty arviointiin. Osoita, onko jo annettuja
-arvosanoja tarpeen korjata. Jos valitset tämän, tarkistuslista piilotetaan opiskelijoilta, kunnes olet korjannut heidän arvosanansa.';
+$string['regrademessage1'] = 'Olet tallentamassa muutoksia tarkistuslistaan, jota on jo käytetty arviointiin. Osoita, onko jo annettuja arvosanoja tarpeen korjata. Jos valitset tämän, tarkistuslista piilotetaan opiskelijoilta, kunnes olet korjannut heidän arvosanansa.';
 $string['regrademessage5'] = 'Olet tallentamassa merkittäviä muutoksia tarkistuslistaan, jota on jo käytetty arviointiin. Arviointikirjan arvoa ei muuteta, mutta tarkistuslista piilotetaan opiskelijoilta, kunnes olet korjannut heidän arvosanansa.';
-$string['regradeoption0'] = 'Ei tarvitse arvioida uudelleen';
-$string['regradeoption1'] = 'Arvioi uudelleen';
+$string['regradeoption0'] = 'Älä merkitse uudelleenarvioitavaksi';
+$string['regradeoption1'] = 'Merkitse uudelleenarvioitavaksi';
 $string['restoredfromdraft'] = 'HUOMAUTUS: Viimeisin tämän osallistujan arviointiyritys ei tallentunut oikein, joten arvosanoista on palautettu luonnokset. Jos haluat kumota nämä muutokset, käytä alla olevaa Peruuta-painiketta.';
 $string['save'] = 'Tallenna';
 $string['savechecklist'] = 'Tallenna tarkistuslista valmiiksi käyttöön';
