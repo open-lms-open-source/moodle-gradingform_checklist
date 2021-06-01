5c6d190ebdd1923d159307c5499b895f0585403e
Fabian Batioja <fabian.batioja@openlms.net>
2021-06-01 22:14:04 +0000

    INT-17116: Bump versions for 3.10

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2020061100;
-$plugin->requires   = 2020061500;
-$plugin->release    = '3.9.3';
+$plugin->version    = 2021060100;
+$plugin->requires   = 2020110900;
+$plugin->release    = '3.10.3';
 $plugin->maturity   = MATURITY_STABLE;
