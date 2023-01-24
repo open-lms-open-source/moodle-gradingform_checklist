0744a25b28b8daa3554e4bd5ee9a134926d73f97
Julian Tovar <julings12@gmail.com>
2023-01-24 10:58:39 -0500

    INT-18643: 4.0 MP1 Version bump

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2022122100;
+$plugin->version    = 2023012400;
 $plugin->requires   = 2022041900;
 $plugin->release    = '4.0.5';
 $plugin->maturity   = MATURITY_STABLE;
