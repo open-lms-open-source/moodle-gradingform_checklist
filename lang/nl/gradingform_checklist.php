e77abcfe8ec74516713081953fbe0f27c8837db5
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2023-03-22 20:03:46 +0000

    INT-18815: Importing nl lang

--- lang/nl/gradingform_checklist.php
+++ lang/nl/gradingform_checklist.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2022 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -25,11 +25,11 @@ $string['addgroup'] = 'Voeg toe aan groep';
 $string['alwaysshowdefinition'] = 'Gebruikers toestaan om een voorbeeld van een checklist te bekijken die in de module wordt gebruikt (anders wordt de checklist alleen zichtbaar na beoordeling)';
 $string['backtoediting'] = 'Terug naar bewerken';
 $string['checked'] = 'Ingeschakeld';
-$string['checkitem'] = 'Markeer volledige bonus voor "{$a}"';
+$string['checkitem'] = 'Markeer volledige bonus voor &quot;{$a}&quot;';
 $string['checklist'] = 'Checklist';
 $string['checklistmapping'] = 'Score volgens regels voor cijferkoppelingen';
-$string['checklistmappingexplained'] = 'De kleinst mogelijke score voor deze checklist is <b>{$a->minscore} punten</b>. Deze wordt omgezet naar het minimumcijfer dat beschikbaar is in deze module (wat nul is, tenzij de schaal wordt gebruikt).
-De maximumscore <b>{$a->maxscore} punten</b> wordt omgezet naar het maximumcijfer.<br />
+$string['checklistmappingexplained'] = 'De kleinst mogelijke score voor deze checklist is <b>{$a->minscore} punten</b>. Deze wordt omgezet naar het minimumcijfer dat beschikbaar is in deze module (wat nul is, tenzij de schaal wordt gebruikt).
+De maximumscore <b>{$a->maxscore} punten</b> wordt omgezet naar het maximumcijfer.<br />
 Tussenliggende scores worden omgezet en afgerond naar het dichtstbijzijnde beschikbare cijfer.<br />
 Als een schaal wordt gebruikt in plaats van een cijfer, wordt de score omgezet naar de schaalelementen alsof dit opeenvolgende gehele getallen waren.';
 $string['checklistoptions'] = 'Opties checklist';
@@ -46,7 +46,7 @@ $string['err_nogroups'] = 'Checklist moet minimaal één groep bevatten';
 $string['err_scoreformat'] = 'Het aantal punten voor elk item moet een geldig positief getal zijn';
 $string['err_scoremax'] = 'Aantal punten voor elk item mag niet groter zijn dan 1000';
 $string['err_totalscore'] = 'Het maximale aantal mogelijke punten bij beoordeling door de checklist moet meer dan nul zijn';
-$string['groupfeedback'] = 'Groepsfeedback voor "{$a}"';
+$string['groupfeedback'] = 'Groepsfeedback voor &quot;{$a}&quot;';
 $string['gradingof'] = '{$a} beoordeling';
 $string['groupadditem'] = 'Voeg item toe';
 $string['groupdelete'] = 'Verwijder groep';
@@ -55,23 +55,24 @@ $string['groupempty'] = 'Klik om groep te bewerken';
 $string['groupmovedown'] = 'Verplaats lager';
 $string['groupmoveup'] = 'Verplaats hoger';
 $string['grouppoints'] = 'Groepspunten';
-$string['groupremark'] = 'Groepsopmerking voor "{$a}"';
+$string['groupremark'] = 'Groepsopmerking voor &quot;{$a}&quot;';
 $string['itemdefinition'] = 'Itemdefinitie';
 $string['itemdelete'] = 'Verwijder item';
 $string['itemempty'] = 'Klik om het item te bewerken';
-$string['itemfeedback'] = 'Feedback voor "{$a}"';
-$string['itemremark'] = 'Itemopmerking voor "{$a}"';
+$string['itemfeedback'] = 'Feedback voor &quot;{$a}&quot;';
+$string['itemremark'] = 'Itemopmerking voor &quot;{$a}&quot;';
 $string['itemscore'] = 'Itemscore';
 $string['name'] = 'Naam';
 $string['needregrademessage'] = 'De checklistdefinitie is gewijzigd nadat deze student werd beoordeeld. De student kan deze checklist pas zien nadat je de checklist hebt beoordeeld en het cijfer heb aangepast.';
 $string['pluginname'] = 'Checklist';
 $string['previewchecklist'] = 'Voorbeeld checklist';
 $string['overallpoints'] = 'Totale aantal punten';
-$string['regrademessage1'] = 'Je staat op het punt om wijzigingen op te slaan in een checklist die al is gebruikt voor beoordeling.  Geef aan of bestaande cijfers moeten worden herzien.  Als je deze optie instelt, wordt de checklist verborgen voor studenten tot hun items opnieuw zijn beoordeeld.';
+$string['regrademessage1'] = 'Je staat op het punt om wijzigingen op te slaan in een checklist die al is gebruikt voor beoordeling.
+Geef aan of bestaande cijfers moeten worden herzien. Als je deze optie instelt, wordt de checklist verborgen voor studenten tot hun items opnieuw zijn beoordeeld.';
 $string['regrademessage5'] = 'Je staat op het punt om belangrijke wijzigingen op te slaan in een checklist die al is gebruikt voor beoordeling. Het cijfer in de cijferlijst wordt niet gewijzigd, maar de checklist wordt verborgen voor studenten tot hun items opnieuw zijn beoordeeld.';
 $string['regradeoption0'] = 'Niet markeren voor opnieuw beoordelen.';
 $string['regradeoption1'] = 'Markeren voor opnieuw beoordelen';
-$string['restoredfromdraft'] = 'OPMERKING: de laatste beoordelingspoging voor deze persoon is niet behoorlijk bewaard en daarom zijn de voorlopige cijfers teruggezet. Als je deze wijzigingen wilt annuleren, klik dan op de knop \'Annuleer\' onderaan.';
+$string['restoredfromdraft'] = 'OPMERKING: de laatste beoordelingspoging voor deze persoon is niet behoorlijk bewaard en daarom zijn de voorlopige cijfers teruggezet. Als je deze wijzigingen wilt annuleren, klik dan op de knop &apos;Annuleer&apos; onderaan.';
 $string['save'] = 'Opslaan';
 $string['savechecklist'] = 'Bewaar checklist en maak deze gereed';
 $string['savechecklistdraft'] = 'Bewaar als concept';
@@ -80,5 +81,5 @@ $string['showitempointseval'] = 'Toon punten voor elk item tijdens evaluatie';
 $string['showitempointstudent'] = 'Toon punten voor elk item aan wie beoordeeld wordt';
 $string['enableitemremarks'] = 'Grader toestaan tekstopmerkingen toe te voegen voor elk item in de checklist';
 $string['enablegroupremarks'] = 'Grader toestaan tekstopmerkingen toe te voegen voor elke groep in de checklist';
-$string['showremarksstudent'] = 'Toon all opmerkingen aan diegenen die beoordeeld worden';
+$string['showremarksstudent'] = 'Toon alle opmerkingen aan diegenen die beoordeeld worden';
 $string['unchecked'] = 'Uitgeschakeld';
