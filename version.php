a9568d7bf119f8f831cddf2082aeecec1015adb9
Bryan Cruz <bryan.cruz@openlms.net>
2023-06-15 10:13:23 -0500

    INT-18870: 4.1 Version bump

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2023041101;
-$plugin->requires   = 2022041900;
-$plugin->release    = '4.0.7';
+$plugin->version    = 2023061500;
+$plugin->requires   = 2022112800;
+$plugin->release    = '4.1.3';
 $plugin->maturity   = MATURITY_STABLE;
