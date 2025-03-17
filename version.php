49fea9d959c21493c964f41e38aa41702eb91982
Fabian Batioja <fabian.batioja@openlms.net>
2025-03-17 18:35:39 -0500

    INT-20711: Bump version 4.4MP2

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2025011000;
+$plugin->version    = 2025031800;
 $plugin->requires   = 2024042200;
 $plugin->release    = '4.4.4';
 $plugin->maturity   = MATURITY_STABLE;
