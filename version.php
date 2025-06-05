62e98f96ff71563994bb99f8498343be427daa55
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2025-06-05 16:40:33 -0500

    INT-20930: Bump version 4.5 SB

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2025042900;
+$plugin->version    = 2025060500;
 $plugin->requires   = 2024100700;
-$plugin->release    = '4.5';
+$plugin->release    = '4.5.4';
 $plugin->maturity   = MATURITY_STABLE;
