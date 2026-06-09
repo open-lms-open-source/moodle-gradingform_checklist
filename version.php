de8d939fd3c0bac1a011a5c01e58a5639bfe0299
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2026-06-09 15:27:20 -0500

    INT-21630: 5.1 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_checklist';
-$plugin->version    = 2026032600;
-$plugin->requires   = 2024100700;
-$plugin->release    = '4.5.4';
+$plugin->version    = 2026060900;
+$plugin->requires   = 2025100600;
+$plugin->release    = '5.1.4';
 $plugin->maturity   = MATURITY_STABLE;
